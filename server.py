a8f259cac669ee849297ac0937945e4cd620f48f
Greg Roodt <groodt@gmail.com>
2011-11-05 09:08:17 +0000

    Tweak the test plan. Pluralise.

---
+++ server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import bottle
+import simplejson
+import inflect
+
+bottle.debug(True)
+p=inflect.engine()
+
+@bottle.post('/bottle')
+def store_bottle():
+    # Extract values from JSON POST body
+    raw_json = bottle.request.body
+    json_string = ''.join(raw_json.readlines())
+    parsed_json = simplejson.loads(json_string)
+    (num_bottles, drink, date, thread) = (parsed_json['bottles'], parsed_json['drink'], \
+                                            parsed_json['date'], parsed_json['thread'])
+    
+    # Print the "99 Bottles" song to the console. Correctly pluralise "bottle".
+    plural_bottles=p.plural("bottle",num_bottles)
+    print("%s %s of %s on the wall. Date=%s Thread=%s" % (num_bottles, plural_bottles, \
+                                                                drink, date, thread))
+    return "Cheers!"
+
+# Run the server
+bottle.run(server="paste", host='localhost', port=9999, reloader=True)
